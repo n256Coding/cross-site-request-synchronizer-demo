bd4b5bbc1ccb1a012468383a766c4ef012fef80d
n256Coding <nishan256@gmail.com>
2018-07-28 14:38:32 +0530

    Initial commit

---
+++ src/main/java/com/coding/crosssiterequestsynchronizerdemo/CrossSiteRequestSynchronizerDemoApplication.java
@@ -0,0 +1,12 @@
+package com.coding.crosssiterequestsynchronizerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossSiteRequestSynchronizerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossSiteRequestSynchronizerDemoApplication.class, args);
+	}
+}
